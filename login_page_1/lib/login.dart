77f9c63eb67d682f4d85706774a52f1ba42ca29f
Jeffin Basheer <jefinbasheer786@gmail.com>
2024-03-28 07:29:50 +0530

    adding text fom field and elevated button

--- login_page_1/lib/login.dart
+++ login_page_1/lib/login.dart
@@ -8,37 +8,69 @@ class Screenlogin extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Stack(
-        children: [
-          Image.asset('assests/images/background.png'),
-          Container(
-              padding: EdgeInsets.all(5),
-              child: Image.asset('assests/images/light-1.png')),
-          Container(
-              alignment: Alignment.topCenter,
-              child: Image.asset('assests/images/light-2.png')),
-          Padding(
-            padding: EdgeInsets.only(left: 300, top: 95),
-            child: Container(
+        child: Stack(
+          children: [
+            Image.asset('assests/images/background.png'),
+            Container(
+                padding: EdgeInsets.all(5),
+                child: Image.asset('assests/images/light-1.png')),
+            Container(
+                alignment: Alignment.topCenter,
+                child: Image.asset('assests/images/light-2.png')),
+            Padding(
+              padding: EdgeInsets.only(left: 300, top: 95),
               child: Image.asset('assests/images/clock.png'),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 200),
-            child: Center(
-              child: Container(
-                  child: Text(
-                'Logiin',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: AutofillHints.birthdayDay,
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold),
-              )),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 200),
+              child: Center(
+                child: Text(
+                  'Logiin',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: AutofillHints.birthdayDay,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-          )
-        ],
-      )),
+            Padding(
+                padding: const EdgeInsets.only(top: 300, left: 10, right: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('enter your valid address'),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        label: Text('password'),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child:
+                          Text('login', style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.black)),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
